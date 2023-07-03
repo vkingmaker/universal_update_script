e3a3c92dde58f4fbbe68ae4beb092b2e5d79f25a
Victor Monday <vkingmaker1@gmail.com>
2023-07-03 19:10:03 -0400

    add a script to backup a folder and its content

--- myscript.sh
+++ myscript.sh
@@ -4,15 +4,19 @@ release_file=/etc/os-release
 logfile=/var/log/updater.log
 errorlog=/var/log/updater_errors.log
 
+check_exit_status() {
+    if [ $? -ne 0]
+    then
+        echo "an error occured, please check the $errorlog file."
+    fi
+}
+
 
 if grep -q "Arch" $release_file
 then
     #The host is based on Arch, run e pacman update command
     sudo pacman -Syu 1>>$logfile 2>>$errorlog 
-    if [ $? -ne 0]
-    then
-        echo "an error occured, please check the $errorlog file."
-    fi
+    check_exit_status();
 fi
 
 if grep -q "Pop" $release_file || grep -q "Ubuntu" $release_file
@@ -20,13 +24,7 @@ then
     # The host is based on Debian or Ubuntu
     # Run the version of the command
     sudo apt update 1>>$logfile 2>>$errorlog
-    if [ $? -ne 0]
-    then
-        echo "an error occured, please check the $errorlog file."
-    fi
+    check_exit_status();
     sudo apt dist-upgrade -y 1>>$logfile 2>>$errorlog
-    if [ $? -ne 0]
-    then
-        echo "an error occured, please check the $errorlog file."
-    fi
+    check_exit_status();
 fi
